bb7303a4323c773887d5f33cdd6fd8346b2f2231
lf14 <54648106+lf14@users.noreply.github.com>
2024-11-19 01:02:50 +0000

    [Favoritos y relaciones]
    -se establecen relaciones con una sola tabla de
    favoritos conta el id de cada planeta, vehiculo
    o personaje. El diagram.png es consistente.

--- src/models.py
+++ src/models.py
@@ -17,7 +17,16 @@ class User(Base):
     password = Column(String(20))
     favorite = relationship('Favorite', back_populates='user')
 
-
+    class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    population = Column(Integer)
+    terrain = Column(String(50))
+    climate = Column(String(50))
+    favorite = relationship('Favorite', back_populates='planet')
+    character = relationship('Character', back_populates='planet')
+    
 class Character(Base):
     __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
@@ -26,18 +35,9 @@ class Character(Base):
     specie = Column(String(10))
     homeworld = Column(Integer, ForeignKey('planet.id'))
     planet = relationship('Planet', back_populates='characters')
+    favorite = relationship('Favorite', back_populates='character')
+   
 
-
-class Planet(Base):
-    __tablename__ = 'planet'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30))
-    population = Column(Integer)
-    terrain = Column(String(50))
-    climate = Column(String(50))
-    characters = relationship('Character', back_populates='planet')
-
-    
 class Vehicle(Base):
     __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
@@ -45,6 +45,26 @@ class Vehicle(Base):
     model = Column(String(100))
     passengers = Column(Integer)
     crew = Column (Integer)
+    favorite = relationship('Favorite', back_populates='vehicle')
+
+
+
+    class Favorite(Base):
+        __tablename__ = 'favorite'
+        id = Column(Integer, primary_key=True)
+
+        user_id = Column(Integer, ForeignKey('user.id'))
+        user = relationship('User', back_populates='favorite')
+
+        character_id = Column(Integer, ForeignKey('character.id'))
+        character = relationship('Character', back_populates='favorite')
+
+        planet_id = Column(Integer, ForeignKey('planet.id'))
+        planet = relationship('Planet', back_populates='favorite')
+
+        vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+        vehicle = relationship('Vehicle', back_populates='favorite')
+
 
 
 
